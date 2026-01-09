97b06ab86a43e3969b77629492222526289f7476
dr00p3r <joancobe64@gmail.com>
2026-01-08 20:56:40 -0500

    feat: customer screen who contains table and form, managing the usage

---
+++ synckuropos_app/src/features/customers/components/CustomersScreen.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { CustomersTable } from './CustomersTable';
+import { CustomerFormDialog } from './CustomerFormDialog';
+import { useCustomers } from '../hooks/useCustomers';
+import type { CustomerWithDebt } from '../services/customerRepository';
+
+export const CustomersScreen: React.FC = () => {
+    const {
+        customers,
+        loading,
+        searchTerm,
+        setSearchTerm,
+        showOnlyWithDebt,
+        setShowOnlyWithDebt,
+        showInactive,
+        setShowInactive,
+        loadCustomers,
+        toggleStatus
+    } = useCustomers();
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedCustomer, setSelectedCustomer] = useState<CustomerWithDebt | undefined>(undefined);
+
+    const openCreate = () => {
+        setSelectedCustomer(undefined);
+        setModalVisible(true);
+    };
+
+    const openEdit = (customer: CustomerWithDebt) => {
+        setSelectedCustomer(customer);
+        setModalVisible(true);
+    };
+
+    return (
+        <div className="h-full min-h-0 flex flex-column">
+            <CustomersTable
+                customers={customers}
+                loading={loading}
+                searchTerm={searchTerm}
+                showOnlyWithDebt={showOnlyWithDebt}
+                showInactive={showInactive}
+                onSearchChange={setSearchTerm}
+                onShowOnlyWithDebtChange={setShowOnlyWithDebt}
+                onShowInactiveChange={setShowInactive}
+                onEdit={openEdit}
+                onToggleStatus={toggleStatus}
+                onCreate={openCreate}
+            />
+
+            <CustomerFormDialog
+                visible={modalVisible}
+                onHide={() => setModalVisible(false)}
+                onSave={loadCustomers}
+                customerToEdit={selectedCustomer}
+            />
+        </div>
+    );
+};
