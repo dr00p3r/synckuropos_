c2b5a3f6c405ee3edc92c3d4966316b46c8321a5
dr00p3r <joancobe64@gmail.com>
2026-01-08 20:57:01 -0500

    feat: index files for exportations

--- synckuropos_app/src/features/customers/index.ts
+++ synckuropos_app/src/features/customers/index.ts
@@ -1,13 +1,4 @@
-// Customers hooks  
-export { useCustomersData } from './hooks/useCustomersData';
-
-// Customers components
-export { default as CustomersScreen } from './components/CustomersScreen';
-export { CustomerModal } from './components/CustomerModal/CustomerModal';
-export { default as SearchControls } from './components/SearchControls';
-export { default as CustomersTable } from './components/CustomersTable/CustomersTable';
-export { default as EmptyState } from './components/EmptyState';
-export { default as LoadingState } from './components/LoadingState';
-
-// Customers types
-export * from './types';
+// Components
+export { CustomersScreen } from './components/CustomersScreen';
+export { CustomersTable } from './components/CustomersTable';
+export { CustomerFormDialog } from './components/CustomerFormDialog';
