0fcd2adaf792f6ce51df66b85556d349c89fc89c
dr00p3r <joancobe64@gmail.com>
2026-01-21 12:25:49 -0500

    fix: unused import

--- synckuropos_app/src/features/sales/components/SalesScreen.tsx
+++ synckuropos_app/src/features/sales/components/SalesScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSalesLogic } from '../hooks/useSalesLogic';
 import { ProductSearch } from './ProductSearch';
 import { SaleItemsTable } from './SaleItemsTable';
